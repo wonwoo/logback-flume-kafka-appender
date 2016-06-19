1c624386f949a8f04a96aca433a75732a245ca43
wonwoo <aoruqjfu@gmail.com>
2016-06-19 21:50:16 +0900

    pom 셋팅 및 테스트 수정

--- src/test/java/me/wonwoo/appender/FlumeKafkaAppenderTest.java
+++ src/test/java/me/wonwoo/appender/FlumeKafkaAppenderTest.java
@@ -14,20 +14,20 @@ import java.net.URL;
 public class FlumeKafkaAppenderTest {
   private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-  @Test
-  public void appenderKafkaTest() {
-    logger.info("kafka1 sink test 123");
-  }
+@Test
+public void appenderKafkaTest() {
+  logger.info("kafka1 sink test");
+}
 
-  @Test
-  public void appenderKafkaError() {
-    try {
-      URL uri = new URL("asdfsadfas");
-      uri.openConnection();
-    } catch (MalformedURLException e) {
-      logger.error("url formed exception ", e);
-    } catch (IOException e) {
-      logger.error("error {} : ", e.toString());
-    }
+@Test
+public void appenderKafkaError() {
+  try {
+    URL uri = new URL("urltest");
+    uri.openConnection();
+  } catch (MalformedURLException e) {
+    logger.error("url formed exception ", e);
+  } catch (IOException e) {
+    logger.error("error {} : ", e.toString());
   }
+}
 }
